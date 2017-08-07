14fbb3cf3028a13c04ba40dd686dfe5182045052
Marcus Anderson <30636662+mjandersondeveloper@users.noreply.github.com>
2017-08-06 23:16:03 -0400

    Page Connect Project
    This project implements the use of $_SESSION, $_SERVER, and $key variables to connect different PHP pages to one another if the link to that specific page is clicked.  Each page will also display the page the user previously visited, and a list of pages the user has visited during that current session.

---
+++ Page5.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+$_SESSION['page'][] = $_SERVER['PHP_SELF'];
+
+echo "The last page you visited was: ".$_SERVER['PHP_SELF'].'<br />';
+echo "List of pages you've visited: ".'<br />'; 
+
+foreach ($_SESSION['page'] as $key) {
+	
+	echo $key.'<br>';
+};
+
+?>
+<a href="page1.php">Page 1</a> 
+<a href="page2.php">Page 2</a> 
+<a href="page3.php">Page 3</a> 
+<a href="page4.php">Page 4</a> 
+<a href="page5.php">Page 5</a> 
