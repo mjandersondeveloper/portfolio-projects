d79018df4de12121cee9eff666e8ed385fdb8d58
Marcus Anderson <30636662+mjandersondeveloper@users.noreply.github.com>
2017-08-06 22:43:56 -0400

    Loop Project
    A basic PHP project that displays "for-loop" functionality.

---
+++ Looping.php
@@ -0,0 +1,19 @@
+<?php
+for ($i=1; $i<=10; $i++) {
+echo $i;
+}
+?>
+
+<?php
+$limit = 10;
+for ($i=1; $i<=$limit; $i++) {
+echo "Hello, world<br>";
+}
+?>
+
+<?php
+$limit = 8;
+for ($i=1; $i<=$limit; $i++) {
+echo "Hello, world<br>";
+}
+?>
